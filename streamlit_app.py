e1d74c0d5187c4770f09b1d1da3a334606a34661
Sajee-AC <119013540+Sajee-AC@users.noreply.github.com>
2022-11-25 12:12:30 -0500

    Testrun on view roles with invalid naming

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,41 @@
 
+import streamlit
+import snowflake.connector
+import pandas
+
+
+
+streamlit.title('Air Canada Webpage')
+
+
+
+streamlit.header('Missing Roles')
+
+#streamlit.multiselect(
+#streamlit.dataframe()
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+
+
+my_cur = my_cnx.cursor()
+
+
+
+my_cur.execute("SELECT * FROM SANDBOX.USR_SAJEEVAN_PARAMSOTHY.ROLES_WITH_INVALID_NAMING")
+myresult = my_cur.fetchall()
+
+
+
+
+streamlit.dataframe(myresult)
+
+
+
+
+#streamlit.header('Check out our Roles:')
+#my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.OUR_ROLES")
+#myresult = my_cur.fetchall()
+#streamlit.multiselect("Pick a user: ", list(my_cur.index))
+#streamlit.dataframe(myresult)
